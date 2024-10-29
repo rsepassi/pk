0a008f58e7c8d20d88b5cf0bb1acd882f02c6549
Ryan Sepassi <rsepassi@gmail.com>
2024-10-28 18:32:44 -0700

    getpass added

---
+++ lib/getpass/include/getpass.h
@@ -0,0 +1,4 @@
+#include <stdint.h>
+#include <stdio.h>
+
+ssize_t getpass(char* pw, size_t maxlen);
---
+++ lib/getpass/src/getpass.c
@@ -0,0 +1,59 @@
+#include "getpass.h"
+
+#include <string.h>
+
+#ifdef _WIN32
+
+#include <windows.h>
+
+ssize_t getpass(char* pw, size_t maxlen) {
+  HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+  DWORD mode = 0;
+  ssize_t nread;
+
+  if (!GetConsoleMode(hStdin, &mode)) return -1;
+  if (!SetConsoleMode(hStdin, mode & (~ENABLE_ECHO_INPUT))) return -1;
+
+  if (fgets(pw, maxlen, stdin) == NULL) {
+    SetConsoleMode(hStdin, mode);
+    return -1;
+  }
+
+  nread = strlen(pw) - 1;
+  pw[nread] = 0;
+
+  // Print a newline since echo is disabled
+  printf("\n");
+  SetConsoleMode(hStdin, mode);
+  return nread;
+}
+
+#else
+
+#include <termios.h>
+
+ssize_t getpass(char* pw, size_t maxlen) {
+  struct termios old, new;
+  ssize_t nread;
+
+  if (tcgetattr(0, &old) != 0) return -1;
+
+  new = old;
+  new.c_lflag &= ~ECHO;
+  new.c_lflag |= ECHONL;
+  if (tcsetattr(0, TCSAFLUSH, &new) != 0) return -1;
+
+  if (fgets(pw, maxlen, stdin) == NULL) {
+    tcsetattr(0, TCSAFLUSH, &old);
+    return -1;
+  }
+
+  nread = strlen(pw) - 1;
+  pw[nread] = 0;
+
+  tcsetattr(0, TCSAFLUSH, &old);
+
+  return nread;
+}
+
+#endif
