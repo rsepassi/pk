beff5fa2e99204794fcc1446dc712378d1d2ae74
Ryan Sepassi <rsepassi@gmail.com>
2024-11-05 14:59:04 -0800

    uvco: co_wait_t, CO_AWAIT, CO_DONE

--- lib/uvco/include/uvco.h
+++ lib/uvco/include/uvco.h
@@ -1,5 +1,24 @@
 #pragma once
 
+#include "stdtypes.h"
 #include "uv.h"
+#include "minicoro.h"
 
+typedef struct {
+  mco_coro *co;
+  bool done;
+} co_wait_t;
+
+#define CO_AWAIT(wait)                                                         \
+  do {                                                                         \
+    while (!(wait)->done)                                                      \
+      mco_yield(mco_running());                                                \
+  } while (0)
+
+#define CO_DONE(wait) do { \
+  (wait)->done = true; \
+  CHECK(mco_resume((wait)->co) == MCO_SUCCESS); \
+} while(0)
+
+void uvco_sleep(uv_loop_t *loop, u64 ms);
 ssize_t uvco_fs_stat(uv_loop_t *loop, uv_fs_t *req, const char *path);
--- lib/uvco/src/uvco.c
+++ lib/uvco/src/uvco.c
@@ -3,24 +3,31 @@
 #include <stdbool.h>
 
 #include "log.h"
-#include "minicoro.h"
-
-typedef struct {
-  mco_coro *co;
-  bool done;
-} co_wait_t;
 
 static void fs_cb(uv_fs_t* req) {
-  co_wait_t* wait = (co_wait_t*)req->data;
-  wait->done = true;
-  CHECK(mco_resume(wait->co) == MCO_SUCCESS);
+  CO_DONE((co_wait_t*)req->data);
 }
+
+static void timer_cb(uv_timer_t *handle) {
+  CO_DONE((co_wait_t*)handle->data);
+}
+
 
 ssize_t uvco_fs_stat(uv_loop_t *loop, uv_fs_t *req, const char *path) {
   co_wait_t wait = {mco_running(), 0};
   req->data = &wait;
   ssize_t rc = uv_fs_stat(loop, req, path, fs_cb);
   if (rc != 0) return rc;
-  while (!wait.done) mco_yield(mco_running());
+  CO_AWAIT(&wait);
   return req->result;
+}
+
+void uvco_sleep(uv_loop_t *loop, u64 ms) {
+  co_wait_t wait = {mco_running(), 0};
+  uv_timer_t timer;
+  uv_timer_init(loop, &timer);
+  timer.data = &wait;
+  int rc = uv_timer_start(&timer, timer_cb, ms, 0);
+  CHECK0(rc);
+  CO_AWAIT(&wait);
 }
