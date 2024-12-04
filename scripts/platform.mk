446e4116ea1e50c68706440c973a496439741bd9
Ryan Sepassi <rsepassi@gmail.com>
2024-12-04 08:47:35 -0800

    mk: add HOST vars

--- scripts/platform.mk
+++ scripts/platform.mk
@@ -1,28 +1,36 @@
 # Cross-platform support
 
 # uname target detection
-ifndef TARGET
+ifndef HOST
 	OS := $(shell uname)
 	ARCH := $(shell uname -m)
 	ifeq ($(OS), Darwin)
 		ifeq ($(ARCH), arm64)
-			export TARGET := aarch64-macos
+			export HOST := aarch64-macos
 		else
-			export TARGET := x86_64-macos
+			export HOST := x86_64-macos
 		endif
 	else
 		ifeq ($(ARCH), arm64)
-			export TARGET := aarch64-linux-musl
+			export HOST := aarch64-linux-musl
 		else
-			export TARGET := x86_64-linux-musl
+			export HOST := x86_64-linux-musl
 		endif
 	endif
+else
+export HOST
+endif
+
+ifndef TARGET
+export TARGET := $(HOST)
 else
 export TARGET
 endif
 
 export TARGET_ARCH := $(word 1, $(subst -, ,$(TARGET)))
 export TARGET_OS   := $(word 2, $(subst -, ,$(TARGET)))
+export HOST_ARCH := $(word 1, $(subst -, ,$(HOST)))
+export HOST_OS   := $(word 2, $(subst -, ,$(HOST)))
 
 # object/executable suffixes
 ifeq ($(TARGET_OS), windows)
