1aaf1aa0288f2cdd33bd1563d4ba40f7b0c7fb55
Ryan Sepassi <rsepassi@gmail.com>
2024-12-06 09:18:31 -0800

    echo: use stdnet, some notes

--- cli/src/echo.c
+++ cli/src/echo.c
@@ -2,71 +2,121 @@
 #include "log.h"
 #include "minicoro.h"
 #include "optparse.h"
+#include "stdnet.h"
 #include "uv.h"
 #include "uvco.h"
 
 extern uv_loop_t* loop;
 
-typedef struct {
-  char ip_buf[INET6_ADDRSTRLEN];
-  Str  ip;
-  int  port;
-} IpStr;
+// 1. Connect to the internet.
+//    Can you send an receive IP packets?
+//    You're on THENET.
+// 2. Connect to a friend.
+//    Don't have one, or don't want to bug one?
+//    Connect to PEER2. PEER2 is everybody's friend.
+//    For convenience, if you've downloaded a PK app, PEER2 is already in your
+//    contact book under PEER2. It's also under the name TOM. If you have a
+//    real friend named Tom, you should probably change this. We have
+//    considered changing this default, but have decided that it's an
+//    important historical monument.
 
-const void* sa_get_in_addr(const struct sockaddr* sa) {
-  switch (sa->sa_family) {
-    case AF_INET: {
-      const struct sockaddr_in* sa_in = (const struct sockaddr_in*)sa;
-      return &sa_in->sin_addr;
-    }
-    case AF_INET6: {
-      const struct sockaddr_in6* sa_in6 = (const struct sockaddr_in6*)sa;
-      return &sa_in6->sin6_addr;
-    }
-    default:
-      return 0;
-  }
-}
+// Root record server has replicas/peers
+//
+// They maintain a single distributed hash table of RecordServer records
+// If you know the authoritative record server for a given public key, you
+// can skip the root record servers altogether. They are there as a convenience
+// and a starting point. They are cooperative and decentralized, but the
+// protocol does not admit of malicious actors, i.e. it is not Byzantine fault
+// tolerant. This is a similar model to Linux distributions' package
+// repositories and mirrors.
 
-static int sa_get_port(const struct sockaddr* sa) {
-  switch (sa->sa_family) {
-    case AF_INET: {
-      const struct sockaddr_in* sa_in = (const struct sockaddr_in*)sa;
-      return ntohs(sa_in->sin_port);
-    }
-    case AF_INET6: {
-      const struct sockaddr_in6* sa_in6 = (const struct sockaddr_in6*)sa;
-      return ntohs(sa_in6->sin6_port);
-    }
-    default:
-      return 0;
-  }
-}
+// Unexpired records may be cached and replicated.
+
+// Alice asks the root RecordServer for Bob's RecordServer
+// Alice asks Bob's RecordServer for Bob's LiveLocation
+// The LiveLocation specifies an address and if the address is publicly
+// accessible or if it is a signaling service.
+
+// What if on the same network? No signaling needed.
+// But that's more in the cooperative use case, right?
+// It's just another "signal" option. If Alice wants to signal Bob, she can
+// broadcast locally and also contact Bob's signaling service.
+
+// Alice and Bob are both online.
+// They want to establish communications over IP.
+//
+// If Alice is unilaterally initiating without Bob's knowledge
+//   If Bob has a publicly acessible IP
+//     Alice contacts Bob there
+//   Else
+//     Alice tries to open an external facing port (PCP, UPnP)
+//     If Bob has a connection to a signaling service
+//       Alice contacts Bob's signaling server
+//     Alice signals Bob on the local network
+//
+// Local network signaling
+//   Publish a signal message on the multicast group
+//   If Bob receives it, Bob will contact Alice on her local address
+//
+// Wide network signaling
+//   Alice contacts Bob's signaling server
+//   Bob's signaling server forwards Alice's address to Bob, and
+//   returns Bob's address to Alice.
+//
+// Alice has Bob's public IP address and port, and vice-versa
+// Alice maintains a connection to Bob's signaling service in case Bob is
+// able to open an external port. If Bob signals Alice, then Alice can contact
+// Bob directly.
+//
+// Alice attempts repeatedly contacting Bob on:
+// * the initially given port
+// * sequentially increasing ports
+// * random ports (birthday)
+//
+// If nothing works, use a relay server
+//   Alice and Bob offered their relay servers, if any, in the initial message
+//
+//
+//
+//
+//
+//
+// Else if Alice and Bob are cooperatively initiating
+//
+// 2 scenarios:
+// 1. Alice is unilaterally initiating without Bob's knowledge.
+// 2. Alice and Bob are cooperatively initiating.
+//
+//
+//
+// Scenario 1:
+// Alice needs to establish Bob's IP.
+// Alice queries the record server for a LiveLocation.
+// Alice sends a message to the LiveLocation
+//
+//
+// Let's say Alice is initiating:
+//   If Bob has a public IP.
+//
+// A signaling server to aid in NAT holepunching
+//
+// Scenario 1:
+//   Alice has Bob's public key
+//   Bob has Alice's public key
+// Scenario 2:
+//   Alice and Bob both have a short code
 
 static void IpStr_log(const IpStr* s, Str tag) {
-  LOG("%.*s=%.*s:%d", (int)tag.len, tag.buf, (int)s->ip.len, s->ip.buf,
+  LOG("%" PRIBytes "=%" PRIBytes ":%d", BytesPRI(tag), BytesPRI(s->ip),
       s->port);
 }
 
-static int IpStr_read(IpStr* out, const struct sockaddr* sa) {
-  const void* addr = sa_get_in_addr(sa);
-  if (addr == NULL)
-    return 1;
-
-  if (inet_ntop(sa->sa_family, addr, out->ip_buf, sizeof(out->ip_buf)) == NULL)
-    return 1;
-  out->ip   = Bytes(out->ip_buf, strlen(out->ip_buf));
-  out->port = sa_get_port(sa);
-  return 0;
-}
-
 static void handle_message(UvcoUdpRecv* recv) {
-  Str msg = Bytes(recv->buf.base, recv->buf.len);
-
   IpStr ip_str;
   CHECK0(IpStr_read(&ip_str, recv->addr));
+  IpStr_log(&ip_str, Str("source"));
 
-  IpStr_log(&ip_str, Str("source="));
+  Str msg = Bytes(recv->buf.base, recv->buf.len);
   LOGS(msg);
 
   CHECK0(uvco_udp_send(recv->udp, &recv->buf, 1, recv->addr));
@@ -103,7 +153,6 @@ int demo_echo(int argc, char** argv) {
   }
 
   CHECK(port > 0);
-  LOG("port=%d", port);
 
   // Create UDP
   uv_udp_t udp;
