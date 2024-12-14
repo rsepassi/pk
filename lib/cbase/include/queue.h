13920d3c490706c3ba2a97401e3b01af2c5511d8
Ryan Sepassi <rsepassi@gmail.com>
2024-12-14 10:33:37 -0800

    cbase: queue fixes, q2 implement

--- lib/cbase/include/queue.h
+++ lib/cbase/include/queue.h
@@ -20,27 +20,48 @@ typedef struct {
 } Queue2;
 
 static inline void q_enq(Queue* q, Node* x) {
-  if (q->tail) {
-    q->tail->next = x;
-  } else {
+  x->next = 0;
+  if (q->head == NULL) {
     q->head = x;
     q->tail = x;
+  } else if (q->head == q->tail) {
+    q->head->next = x;
+    q->tail       = x;
+  } else {
+    q->tail->next = x;
+    q->tail       = x;
   }
 }
 
 static inline Node* q_deq(Queue* q) {
   if (q->head == 0)
     return 0;
-
-  Node* x = q->head;
-  if (q->head == q->tail) {
+  else if (q->head == q->tail) {
+    Node* x = q->head;
     q->head = 0;
     q->tail = 0;
     return x;
+  } else {
+    Node* x = q->head;
+    q->head = q->head->next;
+    return x;
+  }
+}
+
+static inline size_t q_len(Queue* q) {
+  if (q->head == 0)
+    return 0;
+  if (q->head == q->tail)
+    return 1;
+
+  size_t i   = 1;
+  Node*  cur = q->head;
+  while (cur != q->tail) {
+    cur = cur->next;
+    ++i;
   }
 
-  q->head = x->next;
-  return x;
+  return i;
 }
 
 // Drain the queue, running code for each node.
@@ -49,6 +70,98 @@ static inline Node* q_deq(Queue* q) {
   do {                                                                         \
     Node* __tail = (q)->tail;                                                  \
     while (((nvar) = q_deq((q)))) {                                            \
+      code;                                                                    \
+      if ((nvar) == __tail)                                                    \
+        break;                                                                 \
+    }                                                                          \
+  } while (0)
+
+static inline void q2_enq(Queue2* q, Node2* x) {
+  x->next = 0;
+  x->prev = 0;
+  if (q->head == NULL) {
+    q->head = x;
+    q->tail = x;
+  } else if (q->head == q->tail) {
+    q->head->next = x;
+    q->tail       = x;
+  } else {
+    x->prev       = q->tail;
+    q->tail->next = x;
+    q->tail       = x;
+  }
+}
+
+static inline Node2* q2_deq(Queue2* q) {
+  if (q->head == 0)
+    return 0;
+  else if (q->head == q->tail) {
+    Node2* x = q->head;
+    q->head  = 0;
+    q->tail  = 0;
+    return x;
+  } else {
+    Node2* x      = q->head;
+    q->head       = q->head->next;
+    q->head->prev = 0;
+    return x;
+  }
+}
+
+static inline Node2* q2_pop(Queue2* q) {
+  if (q->tail == 0)
+    return 0;
+  else if (q->head == q->tail) {
+    Node2* x = q->tail;
+    q->head  = 0;
+    q->tail  = 0;
+    return x;
+  } else {
+    Node2* x      = q->tail;
+    q->tail       = q->tail->prev;
+    q->tail->next = 0;
+    return x;
+  }
+}
+
+static inline size_t q2_len(Queue2* q) {
+  if (q->head == 0)
+    return 0;
+  if (q->head == q->tail)
+    return 1;
+
+  size_t i   = 1;
+  Node2* cur = q->head;
+  while (cur != q->tail) {
+    cur = cur->next;
+    ++i;
+  }
+
+  return i;
+}
+
+static inline void q2_del(Queue2* q, Node2* n) {
+  if (q->head == n) {
+    q2_deq(q);
+    return;
+  }
+  if (q->tail == n) {
+    q2_pop(q);
+    return;
+  }
+
+  if (n->prev)
+    n->prev->next = n->next;
+  if (n->next)
+    n->next->prev = n->prev;
+}
+
+// Drain the queue, running code for each node.
+// code can re-enqueue, the drain will only run through the original tail.
+#define q2_drain(q, nvar, code)                                                \
+  do {                                                                         \
+    Node2* __tail = (q)->tail;                                                 \
+    while (((nvar) = q2_deq((q)))) {                                           \
       code;                                                                    \
       if ((nvar) == __tail)                                                    \
         break;                                                                 \
