de678c6d1fdf5b7450b69fd4396549e00f6d7c77
Ryan Sepassi <rsepassi@gmail.com>
2024-12-21 12:35:49 -0800

    yash added

---
+++ vendor/yash/src/configm.h
@@ -0,0 +1,9 @@
+#ifndef YASH_CONFIGM_H
+#define YASH_CONFIGM_H
+#define PACKAGE_NAME "yash"
+#define PACKAGE_VERSION "2.57"
+#define PACKAGE_COPYRIGHT "Copyright (C) 2007-2024 magicant"
+#define YASH_DATADIR "/usr/local/share/yash"
+#define LOCALEDIR "/usr/local/share/locale"
+#define DEFAULT_LOADPATH YASH_DATADIR
+#endif
